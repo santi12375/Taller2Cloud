3e4e79d082bb626fba52bb799a4e3d7c6b9e08bc
santiago <santiago_quiceno82171@elpoli.edu.co>
2022-07-17 16:30:33 -0500

    add response to user-service controller

--- user-service/src/main/java/co/com/taller2/userservice/controller/UserController.java
+++ user-service/src/main/java/co/com/taller2/userservice/controller/UserController.java
@@ -1,9 +1,14 @@
 package co.com.taller2.userservice.controller;
 
+import co.com.taller2.userservice.commons.FormatParser;
+import co.com.taller2.userservice.commons.Response;
+import co.com.taller2.userservice.commons.ResponseBuild;
 import co.com.taller2.userservice.persistence.entity.User;
 import co.com.taller2.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 import java.util.List;
 
@@ -13,18 +18,31 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final ResponseBuild builder;
+    private final FormatParser formatParser;
 
     @PostMapping
-    public User save(@RequestBody User user){
-        userService.save(user);
-        return user;
+    public Response save(@Valid @RequestBody User user , BindingResult result){
+        if (result.hasErrors()) {
+            return builder.failed(formatParser.formatMessage(result));
+        }
+        try {
+            userService.save(user);
+        }catch (Exception e){
+            return builder.badRequest();
+        }
+        return builder.success(user);
     }
 
     @DeleteMapping("/{id}")
-    public User delete(@PathVariable("id") Long id){
-        User user = userService.findById(id);
-        userService.delete(user);
-        return user;
+    public Response delete(@PathVariable("id") Long id){
+        try {
+            User user = userService.findById(id);
+            userService.delete(user);
+            return builder.success(user);
+        }catch (Exception e){
+            return builder.badRequest();
+        }
     }
 
     @GetMapping
