4a12166ea6e7a021a22d94665429549f7ececf99
santiago <santiago_quiceno82171@elpoli.edu.co>
2022-07-20 14:32:49 -0500

    add config-service and base of eureka-service

--- user-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (id,name,lastname) VALUES (4,'Santiago','Quiceno');
-INSERT INTO users (id,name,lastname) VALUES (5,'David','Lopez');
-INSERT INTO users (id,name,lastname) VALUES (6,'Natalya','Urrea');
