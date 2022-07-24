75aca0665563c9cc31b55504b7ebdaad2d176dd2
johnelias1 <donniche15@gmail.com>
2022-07-24 17:37:01 -0500

    admin-services

---
+++ admin-service/src/test/java/co/com/taller2/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.taller2.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
